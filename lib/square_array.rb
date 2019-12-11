512fb1bf2eb60c87c047d4e43c34ea4ba273575e
darling-flag-8189 <dwinniford@gmail.com>
2019-12-11 17:13:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0 
   new_numbers = []
   while counter < array.length do
-    array[counter]**
+    new_numbers << array[counter]**(2)
     counter += 1
   end
+  p new_numbers
 end
